f0ac334edf6ccdc6a0358bc733c7f1e01541267e
bhabanip07 <prasadbpu@gmail.com>
2019-10-30 23:08:04 +0530

    added service file for license cond

--- src/app/services/license-conditions.service.ts
+++ src/app/services/license-conditions.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
+import 'rxjs/add/operator/map';
+import {LicenseCondition} from '../model/license-condition';
+import {environment} from '../../environments/environment';
+import { HttpModule } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +19,17 @@ export class LicenseConditionsService {
     { licenseNumber: 'HE4695752', licenseType: 'PERMIT', siteLocation: 'London',issueDate: '10/02/2019', expirationDate: '01/10/2019',renewalDate:'11/11/2019' },
   ];
 
-  constructor() { }
+  constructor(public http: Http) { }
+  url = environment.url
   get() {
     return of(this.fighters);
   }
+  save(model){
+    debugger;
+    return this.http.post(this.url+'/order',model)
+    .map(res=>res.json());
+  };
+
+  // source = interval(4000);
+  // warningCheck = interval(180000);
 }
